3bcf5b9738132522b8e2498aaf93e4121939faf9
In2econd <iaustin7@gmail.com>
2018-03-21 13:37:00 -0400

    Updated formatting

--- pingtool.sh
+++ pingtool.sh
@@ -4,18 +4,18 @@
 
 if [ "$1" == "" ]
 then
-	echo "Pingtool"
-	echo "Network ping tool to check for online hosts"
-	echo ""
-	echo "Usage: ./pingtool.sh [network]"
-	echo "Example: ./pingtool.sh 192.168.1"
+    echo "Pingtool"
+    echo "Network ping tool to check for online hosts"
+    echo ""
+    echo "Usage: ./pingtool.sh [network]"
+    echo "Example: ./pingtool.sh 192.168.1"
 
 else
-	echo "[+] Starting Network Scan"
-	
-	for host in `seq 1 254`; do
-		ping -c 1 -W 1 $1.$host | grep "64 bytes"
-	done
-	
-	echo "[+] Network Scan Complete"
+    echo "[+] Starting Network Scan"
+
+    for host in `seq 1 254`; do
+        ping -c 1 -W 1 $1.$host | grep "64 bytes"
+    done
+
+    echo "[+] Network Scan Complete"
 fi
